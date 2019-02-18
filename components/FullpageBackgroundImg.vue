58e50625da9e8dc741bb6a8027a8e3b0139f9d48
Håvard Lundberg <havard.lundberg@gmail.com>
2019-02-18 21:11:21 +0100

    Initial commit

---
+++ components/FullpageBackgroundImg.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="background">
+  <div class="title-container">
+    <h1>{{title}}</h1>
+  </div>
+  <div class="tagline-container">
+    <template v-for="(tagline,index) in taglines">
+      <span class="tagline" :key="index">{{tagline}}</span><br :key="index" />
+    </template>
+  </div>  
+</div>
+</template>
+
+<style lang="scss" scoped>
+.background { 
+ background: transparent url("~static/2_1.jpg") 50% 0 no-repeat fixed; 
+ background-size: cover;
+ width: 100%;
+ min-height: 100vh;
+}
+
+h1 {
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 500%;
+  text-transform: uppercase;
+  color: #fff;
+  font-weight: 700;
+  letter-spacing: 0.2em;
+  margin-top: 50px;
+}
+
+.tagline-container {
+  margin-top: 20%;
+}
+
+.tagline {
+  text-transform: uppercase;
+  color: #000;
+  opacity: 1;
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 200%;
+  font-weight: 400;
+  background: #fff;
+  padding: 0.4em 0.4em 0.2em 0.4em;
+  line-height: 2.5em;  
+}
+</style>
+
+<script>
+export default {
+  props: ["title", "taglines"],
+  name: "FullpageBackgroundImg"
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <fullpage-background-img title="Save the Fjords" :taglines="taglines" />
+  </section>
+  
+</template>
+
+<script>
+import FullpageBackgroundImg from '~/components/FullpageBackgroundImg.vue'
+
+export default {
+  components: {
+    FullpageBackgroundImg
+  },
+  data() {
+    return {
+      taglines: [
+        "Norway's fjords are in danger",
+        "You can help"
+      ]
+    };
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
